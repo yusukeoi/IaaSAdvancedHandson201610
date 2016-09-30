46fd85475e99ce820f3e6790fc9287e68932d22a
Yusuke Ooi <yusukeoi@gmail.com>
2016-09-30 14:16:14 +0900

    ver 0.9!

---
+++ DownloadAuditLog.ps1
@@ -0,0 +1,19 @@
+﻿# 概要：
+# 指定した期間のアクティビティログをダウンロードする。
+
+####################################################################
+
+# サブスクリプション名
+$subscriptionName = "Hybrid ID"
+
+# ログ取得対象の開始日時
+$startTime = "2016-09-01T00:00"
+$endTime   = "2016-10-01T00:00"
+
+####################################################################
+
+Login-AzureRmAccount
+
+Select-AzureRmSubscription -SubscriptionName $subscriptionName
+
+Get-AzureRmLog -StartTime $startTime -EndTime $endTime
